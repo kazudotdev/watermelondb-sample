0aefbf1396fc22d75c323b1fe2491e99ee77fe95
kazu <48248903+kazudotdev@users.noreply.github.com>
2024-03-28 19:07:17 +0900

    db: add temporary query to get change list

--- packages/db/schema/query/todos.sql
+++ packages/db/schema/query/todos.sql
@@ -1,7 +1,15 @@
--- name: CreateUser :exec
-INSERT INTO app.users (id) VALUES (@id) RETURNING *;
+-- name: Get :exec
+--INSERT INTO app.users (id) VALUES (@id) RETURNING *;
+--
+---- name: GetUser :one
+--SELECT * FROM app.users
+--WHERE id = $1 LIMIT 1;
 
--- name: GetUser :one
-SELECT * FROM app.users
-WHERE id = $1 LIMIT 1;
-
+-- name: GetTodosChangeList :many
+--select json_build_object('created', 
+--json_agg(
+--  jsonb_build_object(
+--    'id', id,
+--    'description', description,
+--  )
+--)) as changes from app.todos where created_at > $1;
