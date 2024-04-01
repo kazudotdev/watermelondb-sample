cc3e41b726862d33b94b29e79009491fb26384ce
kazu <48248903+kazudotdev@users.noreply.github.com>
2024-04-01 18:47:12 +0900

    db: query for registering users, groups at the same time

--- packages/db/schema/query/user.sql
+++ packages/db/schema/query/user.sql
@@ -1,5 +1,13 @@
 -- name: CreateUser :exec
-INSERT INTO app.users (id) VALUES (@id) RETURNING *;
+--WITH new_user AS ( 
+--	INSERT INTO app.users(id) VALUES (@id)
+--	RETURNING id
+-- ), new_group AS (
+--	INSERT INTO app.groups(owner_id)
+--	SELECT id AS owner_id FROM new_user
+--	RETURNING owner_id, id
+--) INSERT INTO app.users_groups (user_id, group_id)
+--SELECT owner_id AS user_id, id as group_id FROM new_group;
 
 -- name: GetUser :one
 SELECT * FROM app.users
